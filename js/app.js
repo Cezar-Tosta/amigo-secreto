dcf6c26fd44343e98d0f880de50f081a8d4b913d
Cezar Tosta <cezar.tosta@hotmail.com>
2024-04-18 00:49:40 -0300

    Validação de dados 3
    Não pode repetir nomes.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ function adicionar(){
         alert('Digite um nome.');
         return;
     }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!')
+        return;
+    };
+
     let lista = document.getElementById("lista-amigos");
     amigos.push(amigo.value);
     if (lista.textContent == ""){
